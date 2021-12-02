3c511757540a69783569ac68d83ac0715eaadad4
EC2 Default User <ec2-user@ip-10-0-0-4.ap-northeast-2.compute.internal>
2021-12-02 02:42:17 +0000

    sms server test complete

---
+++ middleware/db.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const Sequelize = require("sequelize");
+const mybatisMapper = require("mybatis-mapper");
+
+// const sequelize = new Sequelize("mysql://root:root@127.0.0.1:3306");
+// const sequelize = new Sequelize(
+//   "postgres://juandlab:juandlab13258@juandlab.cluster-c6hko0mb11bw.ap-northeast-1.rds.amazonaws.com:5432/juandlab"
+// );
+/*
+const sequelize = new Sequelize(
+  "postgres://sp:standardpass13258@sp.cluster-cfun0a43ytwh.ap-northeast-1.rds.amazonaws.com:5432/sp",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      statement_timeout: 5000,
+      idle_in_transaction_session_timeout: 5000
+    },
+    define: {},
+    pool: {
+      max: 60,
+      min: 0,
+      idle: 10000,
+      acquire: 20000
+    },
+    logging: console.log,
+
+  }
+);
+*/
+//const dbUrl = process.env.DB_URL;
+//const sequelize = new Sequelize(dbUrl);
+const config = require(path.join(__dirname, '..', 'config', 'config.json')).test;
+console.log("config",config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+const sqlPath = path.join(__dirname, "..", ".", `/sql`);
+
+mybatisMapper.createMapper([`${sqlPath}/sms.xml`]);
+
+var db = async function (req, res, next) {
+  req.sequelize = sequelize;
+  req.mybatisMapper = mybatisMapper;
+  next();
+};
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,83 @@
+//------------------------------------------------
+// require
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require(`${__dirname}/middleware/db`));
+
+//app.use(httpMiddleware);
+//app.use(awsMiddleware);
+// app.use(require(`${__dirname}/middleware/googleapi`));
+// app.use(require(`${__dirname}/middleware/appsyncExcuter`));
+
+// // router
+
+app.get("/",async function (req, res) {
+  
+    res.send("Hello XY----");
+});
+
+app.post("/",async function (req, res) {
+    const aaa = req.body.aaa;
+    res.json({ success_yn: true, msg: "success" ,aaa });
+});
+
+
+app.post("/sms-insert",async function (req, res) {
+    let insertQuery1;
+    console.log("req.body",req.body);
+    const orig_addr = req.body.params.orig_addr;
+    const dest_addr = req.body.params.dest_addr;
+    const call_back = req.body.params.call_back;
+    const sms_message = req.body.params.sms_message;
+    const user_id = req.body.params.user_id;
+
+    if (orig_addr == null || dest_addr == null ||call_back == null ||sms_message == null ||user_id == null ) {
+        res.status(401).send({ success_yn: false, msg: "bad parameter" });
+        return;
+    }
+    insertQuery1 = req.mybatisMapper.getStatement(
+        "SMS",
+        "SMS_INSERT.INSERT",
+        {
+            user_id,
+            orig_addr,
+            dest_addr,
+            call_back,
+            sms_message,
+        },
+        { language: "sql", indent: "  " }
+    );
+    try {
+        var result = await req.sequelize.query(insertQuery1, {
+            type: req.sequelize.QueryTypes.INSERT,
+        });
+
+        res.json({ success_yn: true, msg: "success"  });
+    } catch (error) {
+        res.status(403).send({ success_yn: false, error: error });
+        console.log("query: error", error);
+        return;
+    }
+});
+
+
+
+app.listen(3000, function () {
+  console.log("App started");
+});
+
+module.exports = app;
+
